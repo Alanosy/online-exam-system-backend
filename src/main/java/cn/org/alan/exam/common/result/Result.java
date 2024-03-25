39001c8031a3bde9da12e09a7d1b158f4a02949d
魏进 <3413105907@qq.com>
2024-03-25 13:09:44 +0800

    返回响应体搭建

--- src/main/java/cn/org/alan/exam/common/result/Result.java
+++ src/main/java/cn/org/alan/exam/common/result/Result.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
 @Data
 public class Result<T> implements Serializable {
 
-    private String code;
+    private Integer code;
 
     private T data;
 
@@ -25,41 +25,41 @@ public class Result<T> implements Serializable {
 
     public static <T> Result<T> success(T data) {
         Result<T> result = new Result<>();
-        result.setCode(ResultCode.SUCCESS.getCode());
-        result.setMsg(ResultCode.SUCCESS.getMsg());
+        result.setCode(1);
+        result.setMsg("请求成功！");
         result.setData(data);
         return result;
     }
 
-    public static <T> Result<T> failed() {
-        return result(ResultCode.SYSTEM_EXECUTION_ERROR.getCode(), ResultCode.SYSTEM_EXECUTION_ERROR.getMsg(), null);
+    public static <T> Result<T> failed(ResultCode resultCode) {
+        return result(0,resultCode.getMsg() , null);
     }
 
-    public static <T> Result<T> failed(String msg) {
-        return result(ResultCode.SYSTEM_EXECUTION_ERROR.getCode(), msg, null);
-    }
+//    public static <T> Result<T> failed(String msg) {
+//        return result(ResultCode.SYSTEM_EXECUTION_ERROR.getCode(), msg, null);
+//    }
+//
+//    public static <T> Result<T> judge(boolean status) {
+//        if (status) {
+//            return success();
+//        } else {
+//            return failed();
+//        }
+//    }
+//
+//    public static <T> Result<T> failed(IResultCode resultCode) {
+//        return result(resultCode.getCode(), resultCode.getMsg(), null);
+//    }
+//
+//    public static <T> Result<T> failed(IResultCode resultCode, String msg) {
+//        return result(resultCode.getCode(), msg, null);
+//    }
+//
+//    private static <T> Result<T> result(IResultCode resultCode, T data) {
+//        return result(resultCode.getCode(), resultCode.getMsg(), data);
+//    }
 
-    public static <T> Result<T> judge(boolean status) {
-        if (status) {
-            return success();
-        } else {
-            return failed();
-        }
-    }
-
-    public static <T> Result<T> failed(IResultCode resultCode) {
-        return result(resultCode.getCode(), resultCode.getMsg(), null);
-    }
-
-    public static <T> Result<T> failed(IResultCode resultCode, String msg) {
-        return result(resultCode.getCode(), msg, null);
-    }
-
-    private static <T> Result<T> result(IResultCode resultCode, T data) {
-        return result(resultCode.getCode(), resultCode.getMsg(), data);
-    }
-
-    private static <T> Result<T> result(String code, String msg, T data) {
+    private static <T> Result<T> result(Integer code, String msg, T data) {
         Result<T> result = new Result<>();
         result.setCode(code);
         result.setData(data);
@@ -67,7 +67,7 @@ public class Result<T> implements Serializable {
         return result;
     }
 
-    public static boolean isSuccess(Result<?> result) {
-        return result != null && ResultCode.SUCCESS.getCode().equals(result.getCode());
-    }
+//    public static boolean isSuccess(Result<?> result) {
+//        return result != null && ResultCode.SUCCESS.getCode().equals(result.getCode());
+//    }
 }
